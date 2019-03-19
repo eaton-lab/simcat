6fb0ee219eacb5fc385c39b92b0504acbf36f136
eaton-lab <de2356@columbia.edu>
2019-03-19 00:45:58 -0400

    matrix plotting funcs

--- simcat/plot.py
+++ simcat/plot.py
@@ -1,37 +1,15 @@
 #!/usr/bin/env python
 
 
+import itertools
 import numpy as np
 import toyplot
 
+from .utils import get_all_admix_edges, INVARIANTS, SimcatError
+from .utils import ABBA_IDX, BABA_IDX, FIXED_IDX
 
-_ISTRING = """
-    AAAA AAAC AAAG AAAT  AACA AACC AACG AACT  AAGA AAGC AAGG AAGT  AATA AATC AATG AATT
-    ACAA ACAC ACAG ACAT  ACCA ACCC ACCG ACCT  ACGA ACGC ACGG ACGT  ACTA ACTC ACTG ACTT
-    AGAA AGAC AGAG AGAT  AGCA AGCC AGCG AGCT  AGGA AGGC AGGG AGGT  AGTA AGTC AGTG AGTT
-    ATAA ATAC ATAG ATAT  ATCA ATCC ATCG ATCT  ATGA ATGC ATGG ATGT  ATTA ATTC ATTG ATTT
-
-    CAAA CAAC CAAG CAAT  CACA CACC CACG CACT  CAGA CAGC CAGG CAGT  CATA CATC CATG CATT
-    CCAA CCAC CCAG CCAT  CCCA CCCC CCCG CCCT  CCGA CCGC CCGG CCGT  CCTA CCTC CCTG CCTT
-    CGAA CGAC CGAG CGAT  CGCA CGCC CGCG CGCT  CGGA CGGC CGGG CGGT  CGTA CGTC CGTG CGTT
-    CTAA CTAC CTAG CTAT  CTCA CTCC CTCG CTCT  CTGA CTGC CTGG CTGT  CTTA CTTC CTTG CTTT
-
-    GAAA GAAC GAAG GAAT  GACA GACC GACG GACT  GAGA GAGC GAGG GAGT  GATA GATC GATG GATT
-    GCAA GCAC GCAG GCAT  GCCA GCCC GCCG GCCT  GCGA GCGC GCGG GCGT  GCTA GCTC GCTG GCTT
-    GGAA GGAC GGAG GGAT  GGCA GGCC GGCG GGCT  GGGA GGGC GGGG GGGT  GGTA GGTC GGTG GGTT
-    GTAA GTAC GTAG GTAT  GTCA GTCC GTCG GTCT  GTGA GTGC GTGG GTGT  GTTA GTTC GTTG GTTT
-
-    TAAA TAAC TAAG TAAT  TACA TACC TACG TACT  TAGA TAGC TAGG TAGT  TATA TATC TATG TATT
-    TCAA TCAC TCAG TCAT  TCCA TCCC TCCG TCCT  TCGA TCGC TCGG TCGT  TCTA TCTC TCTG TCTT
-    TGAA TGAC TGAG TGAT  TGCA TGCC TGCG TGCT  TGGA TGGC TGGG TGGT  TGTA TGTC TGTG TGTT
-    TTAA TTAC TTAG TTAT  TTCA TTCC TTCG TTCT  TTGA TTGC TTGG TTGT  TTTA TTTC TTTG TTTT
-"""
-
-
-INVARIANTS = np.array(_ISTRING.strip().split()).reshape(16, 16)
-
-
-PALETTE = toyplot.color.brewer.palette("Set3", count=5)
+# a series of colormaps 
+PALETTE = toyplot.color.brewer.palette("Set2", count=5)
 COLORMAPS = [
     toyplot.color.LinearMap(
         toyplot.color.Palette([
@@ -41,13 +19,31 @@ COLORMAPS = [
         domain_min=0, domain_max=1,
     ) for i in range(5)
 ]
+DIVERGING = toyplot.color.LinearMap(toyplot.color.brewer.palette("BlueRed"))
 
 
 
-## plotting functions
+# # if it is a model object
+# if hasattr(data, "nreps"):
+#     if data.ntests > 1:
+#         raise SimcatError("Cannot plot model with multiple tests.")
+#     if data.nreps > 1:
+#         counts = data.counts.mean(axis=0)
+#     else:
+#         counts = data.counts[0]
+# elif isinstance(data, np.ndarray):
+#     if len(data.shape) > 3:
+#         counts = data.mean(axis=0)
+#     else:
+#         counts = data[0]
+# else:
+#     raise SimcatError("data type not recognized")
+
+
 def draw_count_matrix(
-    count_matrix, 
-    norm_variants=True, 
+    matrix=None, 
+    normalize=True,
+    mask_fixed_values=True, 
     show_invariants=True,
     **kwargs):
     """
@@ -55,7 +51,7 @@ def draw_count_matrix(
 
     Parameters:
     -----------
-    count_matrix: ndarray or (ndarray, colormap)
+    data: ndarray
         The array to be colormapped
 
     norm_variants: bool
@@ -68,16 +64,30 @@ def draw_count_matrix(
     ---------------------
     height, width, font-size.
     """
+
+    # if no matrix then make empty
+    if matrix is None:
+        counts = np.zeros((16, 16), dtype=int)   
+    else:
+        counts = matrix
+
+    # check shape
+    if counts.shape != (16, 16):
+        raise SimcatError("matrix shape must be (16, 16)")
+
     # set invariant cells to max
-    invs = [(0, 0), (5, 5), (10, 10), (15, 15)]
-    if norm_variants:
-        for cell in invs:
-            count_matrix[cell] = 0
-        count_matrix = count_matrix / count_matrix.max()
+    if mask_fixed_values:
+        counts = counts.copy()
+        for idx in FIXED_IDX:
+            counts[idx] = 0
+        
+    # normalize counts
+    if normalize and (matrix is not None):
+        counts = counts / counts.max()
 
     # if no user provided colormap use the gradient color[0]
-    if not isinstance(count_matrix, (tuple, list)):
-        count_matrix = (count_matrix, COLORMAPS[0])
+    if not isinstance(counts, (tuple, list)):
+        counts = (counts, COLORMAPS[0])
 
     # canvas arguments
     ckwargs = {
@@ -94,7 +104,7 @@ def draw_count_matrix(
 
     # create canvas and table
     canvas = toyplot.Canvas(**ckwargs)
-    table = canvas.matrix(count_matrix, margin=20)
+    table = canvas.matrix(counts, margin=20)
 
     # style the table
     table.cells.grid.hlines[2:-2, 2:-2] = "single"
@@ -107,26 +117,302 @@ def draw_count_matrix(
     table.body.cell[...].lstyle = skwargs
 
     # fill hidden cells with grey
-    if norm_variants:
+    if normalize:
         table.body.cell[0, 0].style = {"fill": "lightgrey"}
         table.body.cell[5, 5].style = {"fill": "lightgrey"}
         table.body.cell[10, 10].style = {"fill": "lightgrey"}
         table.body.cell[15, 15].style = {"fill": "lightgrey"}    
+
+    # if no matrix then color ABBA BABA
+    if matrix is None:
+        for pair in BABA_IDX:
+            table.body.cell[pair].style = {"fill": toyplot.color.Palette()[0]}
+        for pair in ABBA_IDX:
+            table.body.cell[pair].style = {"fill": toyplot.color.Palette()[1]}
+
     return canvas, table
 
 
-def draw_quartet_matrices(ttree, ndarray, **kwargs):
+
+def draw_five_matrix_comparison(model1, model2, domain_min=None, domain_max=None, **kwargs):
     """
-    Plot quartet matrices for the 5-taxon example.
+    Plot tree, admixture and quartet matrices for the 5-taxon example.
     """
 
-    # scale tree to root height=3
-    ttree = ttree.jitter.scale_root_height(3)
+    # check for ntests, only allowed nreps here.
+    if (model1.ntests > 1) or (model2.ntests > 1):
+        raise SimcatError("Cannot plot for objects with ntests > 1")
+
+    if (model1.nreps > 1) or (model2.nreps > 1):
+        counts1 = model1.counts.mean(axis=0)
+        counts2 = model2.counts.mean(axis=0)
+    else:
+        counts1 = model1.counts[0]
+        counts2 = model2.counts[0]
+
+    # tree and admixture edges from models.
+    ttree = model1.tree
+    admixture_edges = list(model1.admixture_edges) + list(model2.admixture_edges)
+
+    # get abs values
+    diff = counts1.astype(np.int64) - counts2.astype(np.int64)
+
+    # set colormap 
+    colormap = toyplot.color.LinearMap(
+        toyplot.color.brewer.palette("BlueRed"), 
+        center=0.0,
+        domain_max=(domain_max if domain_max else diff.max()),
+        domain_min=(domain_min if domain_min else diff.min()),
+    )
 
     # canvas argumnets
     ckwargs = {
-        "width": 2400, 
-        "height": 400,
+        "width": 2600, 
+        "height": 1000,
+    }
+    ckwargs.update({i: j for i, j in kwargs.items() if i in ckwargs})
+
+    # tree styling
+    tkwargs = {
+        "tree_style": "d", 
+        "orient": "down",
+        "edge_style": {"stroke-width": 12},
+    }
+    tkwargs.update({i: j for i, j in kwargs.items() if i in tkwargs})
+
+    # build canvas
+    canvas = toyplot.Canvas(**ckwargs)
+
+    # get quartet sets
+    qsets = [set(i) for i in itertools.combinations(range(5), 4)]
+    qfull = set(range(5))
+    
+    # get node coordinates
+    coords = ttree.get_node_coordinates()
+    edges = get_all_admix_edges(ttree, 0.5, 0.5)
+
+    # add tree to canvas
+    x = 50
+    for idx in range(5):
+        
+        tipcols = np.array(["#262626"] * 5)
+        notidx = list(qfull - qsets[idx])[0]
+        
+        # draw tree
+        axes = canvas.cartesian(bounds=(x + 50, x + 450, 100, 400))
+        ttree.draw(
+            axes=axes, 
+            edge_colors=ttree.get_edge_values_from_dict({
+                tuple(ttree.get_tip_labels()): '#262626',
+                str(notidx): "lightgrey",
+            }),
+            **tkwargs)
+
+        # tip dots
+        axes.scatterplot(
+            range(5), [0] * 5, color=[i for i in PALETTE], size=10)
+        axes.y.domain.min = 0
+        axes.show = False
+        
+        # admixture edge
+        if admixture_edges:
+            for edge in admixture_edges:
+                src, dest = edge[0], edge[1]                
+                xsrc, xdest = coords[src][0], coords[dest][0]
+                height = edges[(src, dest)][0]
+                arrow = toyplot.marker.create(
+                    shape=">",
+                    size=25,
+                    mstyle={"fill": "grey", "stroke": "none"},
+                )
+                axes.graph(
+                    np.array([(0, 1)]),
+                    vcoordinates=[(xdest, height), (xsrc, height)],
+                    tmarker=arrow,
+                    estyle={
+                        "stroke-width": 7, 
+                        "stroke": "grey",
+                    }
+                )
+        
+        # tip dots
+        tipcols[notidx] = "grey"
+        axes.scatterplot(
+            range(5), [0] * 5,
+            size=30,
+            color=tipcols,
+        )
+
+        # draw matrix
+        table = canvas.matrix(
+            (diff[idx], colormap),
+            lshow=0, 
+            tshow=0, 
+            margin=20, 
+            bounds=(x, x + 500, 450, 950),
+        )
+
+        # update hover values of table
+        for i in range(16):
+            for j in range(16):
+                table.body.cell[i, j].title = "{}: {:.3f}".format(
+                    INVARIANTS[i, j], diff[idx][i, j])
+
+        # shift on x axis to next plot
+        x += 500
+    return canvas
+
+
+
+def draw_five_taxon_matrix(model, **kwargs):
+    """
+    Plot count matrices and tree model for simulation with 5 tips.
+
+    Parameters:
+    -----------
+    model: ndarray
+        The array to be colormapped
+    
+    kwargs: dict
+        Arguments to toyplot.canvas
+
+    Useful kwarg options:
+    ---------------------
+    height, width, font-size.
+    """
+    # get tree from model
+    ttree = model.tree
+    if len(ttree) > 5:
+        raise SimcatError("This plotting function is only for 5 tip trees")
+    
+    # check for ntests, only allowed nreps here.
+    if model.ntests > 1:
+        raise SimcatError("Cannot plot for objects with ntests > 1")
+
+    if (model.nreps > 1):
+        counts = model.counts.mean(axis=0)
+    else:
+        counts = model.counts[0]
+
+    # set colormap 
+    colormap = toyplot.color.LinearMap(
+        toyplot.color.brewer.palette("BlueRed"), 
+        center=0.0,
+        domain_max=counts.max(),
+        domain_min=counts.min(),
+    )
+
+    # canvas argumnets
+    ckwargs = {
+        "width": 2600, 
+        "height": 1000,
+    }
+    ckwargs.update({i: j for i, j in kwargs.items() if i in ckwargs})
+
+    # tree styling
+    tkwargs = {
+        "tree_style": "d", 
+        "orient": "down",
+        "edge_style": {"stroke-width": 12},
+    }
+    tkwargs.update({i: j for i, j in kwargs.items() if i in tkwargs})
+
+    # build canvas
+    canvas = toyplot.Canvas(**ckwargs)
+
+    # get quartet sets
+    qsets = [set(i) for i in itertools.combinations(range(5), 4)]
+    qfull = set(range(5))
+    
+    # get node coordinates
+    coords = ttree.get_node_coordinates()
+    edges = get_all_admix_edges(ttree, 0.5, 0.5)
+
+    # add tree to canvas
+    x = 50
+    for idx in range(5):
+        
+        tipcols = np.array(["#262626"] * 5)
+        notidx = list(qfull - qsets[idx])[0]
+        
+        # draw tree
+        axes = canvas.cartesian(bounds=(x + 50, x + 450, 100, 400))
+        ttree.draw(
+            axes=axes, 
+            edge_colors=ttree.get_edge_values_from_dict({
+                tuple(ttree.get_tip_labels()): '#262626',
+                str(notidx): "lightgrey",
+            }),
+            **tkwargs)
+
+        # tip dots
+        axes.scatterplot(
+            range(5), [0] * 5, color=[i for i in PALETTE], size=10)
+        axes.y.domain.min = 0
+        axes.show = False
+        
+        # admixture edge
+        if model.admixture_edges:
+            for edge in model.admixture_edges:
+                src, dest = edge[0], edge[1]                
+                xsrc, xdest = coords[src][0], coords[dest][0]
+                height = edges[(src, dest)][0]
+                arrow = toyplot.marker.create(
+                    shape=">",
+                    size=25,
+                    mstyle={"fill": "grey", "stroke": "none"},
+                )
+                axes.graph(
+                    np.array([(0, 1)]),
+                    vcoordinates=[(xdest, height), (xsrc, height)],
+                    tmarker=arrow,
+                    estyle={
+                        "stroke-width": 7, 
+                        "stroke": "grey",
+                    }
+                )
+        
+        # tip dots
+        tipcols[notidx] = "grey"
+        axes.scatterplot(
+            range(5), [0] * 5,
+            size=30,
+            color=tipcols,
+        )
+
+        # draw matrix
+        table = canvas.matrix(
+            (counts[idx], colormap),
+            lshow=0, 
+            tshow=0, 
+            margin=20, 
+            bounds=(x, x + 500, 450, 950),
+        )
+
+        # update hover values of table
+        for i in range(16):
+            for j in range(16):
+                table.body.cell[i, j].title = "{}: {:.3f}".format(
+                    INVARIANTS[i, j], counts[idx][i, j])
+
+
+        # shift on x axis to next plot
+        x += 500
+    return canvas
+
+
+
+def _draw_five_taxon_data(ttree, admixture_edges, counts, baseline=None, **kwargs):
+    """
+    Plot tree, admixture and quartet matrices for the 5-taxon example.
+    """
+    # scale tree to root height=3
+    ttree = ttree.mod.node_scale_root_height(3)
+
+    # canvas argumnets
+    ckwargs = {
+        "width": 2600, 
+        "height": 1000,
         "style": {"background-color": "black"},
     }
     ckwargs.update({i: j for i, j in kwargs.items() if i in ckwargs})
@@ -135,27 +421,82 @@ def draw_quartet_matrices(ttree, ndarray, **kwargs):
     tkwargs = {
         "tree_style": "d", 
         "orient": "down",
-        "edge_style": {"stroke-width": 6, "stroke": "white"},
+        "edge_style": {"stroke-width": 12, "stroke": "white"},
     }
     tkwargs.update({i: j for i, j in kwargs.items() if i in tkwargs})
 
     # build canvas
     canvas = toyplot.Canvas(**ckwargs)
 
-    # add tree to canvas
-    axes = canvas.cartesian(bounds=(50, 350, 50, 325))
-    ttree.draw(axes=axes, **tkwargs)
-    axes.scatterplot(range(5), [-0.5] * 5, color=[i for i in PALETTE], size=30)
+    # get quartet sets
+    qsets = [set(i) for i in itertools.combinations(range(5), 4)]
+    qfull = set(range(5))
+    
+    # get node coordinates
+    coords = ttree.get_node_coordinates()
+    edges = get_all_admix_edges(ttree, 0.5, 0.5)
 
-    # add matrices
-    x = 400    
-    for idx in range(ndarray.shape[0]):
+    # add tree to canvas
+    x = 50
+    for idx in range(5):
+        
+        tipcols = np.array(["white"] * 5)
+        notidx = list(qfull - qsets[idx])[0]
+        
+        # draw tree
+        axes = canvas.cartesian(bounds=(x + 50, x + 450, 100, 400))
+        ttree.draw(
+            axes=axes, 
+            edge_colors=ttree.get_edge_values_from_dict({
+                tuple(ttree.get_tip_labels()): 'white',
+                str(notidx): "grey",
+            }),
+            **tkwargs)
+        axes.scatterplot(
+            range(5), [-0.5] * 5, color=[i for i in PALETTE], size=20)
+        axes.y.domain.min = 0
+        axes.show = False
+        
+        # admixture edge
+        if admixture_edges:
+            for edge in admixture_edges:
+                src, dest = edge[0], edge[1]                
+                xsrc, xdest = coords[src][0], coords[dest][0]
+                height = edges[(src, dest)][0]
+                arrow = toyplot.marker.create(
+                    shape=">",
+                    size=25,
+                    mstyle={"fill": "white", "stroke": "none"},
+                )
+                axes.graph(
+                    np.array([(0, 1)]),
+                    vcoordinates=[(xdest, height), (xsrc, height)],
+                    tmarker=arrow,
+                    estyle={
+                        "stroke-width": 7, 
+                        #"stroke-dasharray": "5,5", 
+                        "stroke": "white",
+                    }
+                )
+        
+        # tip dots
+        tipcols[notidx] = "grey"
+        axes.scatterplot(
+            range(5), [0] * 5,
+            size=30,
+            color=tipcols,
+        )
+
+        if baseline:
+            counts = abs(counts - baseline)
+        
+        # draw matrix
         canvas.matrix(
-            (ndarray[idx], COLORMAPS[idx]), 
+            (counts[idx], COLORMAPS[idx]), 
             lshow=0, 
             tshow=0, 
             margin=20, 
-            bounds=(x, x + 400, 0, 400),
+            bounds=(x, x + 500, 450, 950),
         )
-        x += 400
+        x += 500
     return canvas
