713de24c6e00b7360a3ebcf97b8d13c862b847a2
eaton-lab <de2356@columbia.edu>
2019-03-28 11:13:27 -0400

    ml notes

--- simcat/ml.py
+++ simcat/ml.py
@@ -3,3 +3,13 @@
 """
 Machine learning code for analyzing database info...
 """
+
+
+"""
+## Ideas 
+1. Visualization and feature extraction: tSNE (base PCA or NMF)
+2. Visualization of features: NMF or ExtraTrees.
+3. Classification: ExtraTrees using raw or extracted features.
+4. Classification + linear fit (e.g., supervised with edges and props.)
+5. ...
+"""
