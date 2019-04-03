dbf931925c80b7be8b5cfcc25c579b77321ba0be
eaton-lab <de2356@columbia.edu>
2019-04-03 16:33:28 -0400

    added Analysis class and notebook

--- simcat/__init__.py
+++ simcat/__init__.py
@@ -5,7 +5,8 @@ from .Model import Model
 from .Database import Database
 from . import utils
 from . import plot
+from .ml import Analysis
 
 # dunders
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __authors__ = "Patrick McKenzie and Deren Eaton"
