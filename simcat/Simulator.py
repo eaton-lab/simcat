89129ef2097285ebd623ec7eb5f8d6e34b1f16ad
eaton-lab <de2356@columbia.edu>
2019-11-28 16:00:48 -0500

    >=30% speed improvement using threading within engines

--- simcat/Simulator.py
+++ simcat/Simulator.py
@@ -9,12 +9,17 @@ from __future__ import print_function
 from builtins import range
 
 import h5py
-import itertools
-
 import ipcoal
 import toytree
 import numpy as np
 from .utils import get_snps_count_matrix
+
+# py3 only
+try:
+    from concurrent.futures import ThreadPoolExecutor
+    PY3 = True
+except ImportError:
+    PY3 = False
 
 
 
@@ -37,7 +42,10 @@ class IPCoalWrapper:
         # fill the vector of simulated data for .counts
         if run:
             # infer count matrices on slice of data    
-            self.run()
+            if PY3:
+                self.run_threads()
+            else:
+                self.run()
 
             # normalize counts while in stacked format?
             pass
@@ -96,11 +104,62 @@ class IPCoalWrapper:
             self.ntips = len(self.tree)
 
             # storage SNPs in STACKED matrix so we can compute stacked stats
-            self.nquarts = sum(
-                1 for i in itertools.combinations(range(self.ntips), 4))
+            self.nquarts = io5.attrs["nquarts"]
             self.nvalues = self.slice1 - self.slice0
             self.counts = np.zeros(
                 (self.nvalues, self.nquarts, 16, 16), dtype=np.int64) 
+
+
+    def run_threads(self):
+        """
+        Multithread it
+        """
+
+        with ThreadPoolExecutor(max_workers=2) as executor:
+
+            # run simulations
+            for idx in range(self.nvalues):
+
+                # modify the tree if ...
+                tree = self.tree.mod.node_slider(
+                    prop=0.25, seed=self.slide_seeds[idx])
+
+                # set Nes default and override on internal nodes with stored vals
+                tree = tree.set_node_values("Ne", default=1e5)
+                nes = iter(self.node_Nes[idx])
+                for node in tree.treenode.traverse():
+                    if not node.is_leaf():
+                        node.Ne = next(nes)
+
+                # get admixture tuples (only supports 1 edge like this right now)
+                admix = (
+                    int(self.admixture[idx, 0]),
+                    int(self.admixture[idx, 1]),
+                    0.5,
+                    self.admixture[idx, 2],
+                )
+
+                # build ipcoal Model object
+                model = ipcoal.Model(
+                    tree=tree,
+                    admixture_edges=[admix],
+                    Ne=None,
+                    )
+
+                # simulate genealogies and snps
+                nsnps = int(self.nsnps / 2)
+                res = [
+                    executor.submit(thread_sim_snps, *(model, nsnps))
+                    for i in range(2)
+                ]
+
+                # TODO: ipcoal converter not fastest possible
+                resms = [i.result() for i in res]
+                matrix = np.concatenate(resms, axis=1)
+                mat = get_snps_count_matrix(tree, matrix)
+
+                # store results
+                self.counts[idx] = mat
 
 
 
@@ -145,3 +204,9 @@ class IPCoalWrapper:
 
             # store results
             self.counts[idx] = mat
+
+
+
+def thread_sim_snps(model, nsnps):
+    model.sim_snps(nsnps)
+    return model.seqs
