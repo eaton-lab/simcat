05fa7e8752b53fac7a9401b12fdbce1ab1a72d66
eaton-lab <de2356@columbia.edu>
2019-12-03 18:32:01 -0500

    count matrix not requiring .T

--- simcat/jitted.py
+++ simcat/jitted.py
@@ -6,17 +6,28 @@ import numpy as np
 from numba import njit
 
 
-@njit()
 def count_matrix_int(quartsnps):
     """
     return a 16x16 matrix of site counts from snparr
     """
     arr = np.zeros((16, 16), dtype=np.int64)
-    add = np.int64(1)
-    for idx in range(quartsnps.shape[0]):
-        i = quartsnps[idx, :]
-        arr[(4 * i[0]) + i[1], (4 * i[2]) + i[3]] += add
+    for idx in range(quartsnps.shape[1]):
+        i = quartsnps[:, idx]
+        arr[(4 * i[0]) + i[1], (4 * i[2]) + i[3]] += 1
     return arr
+
+
+# @njit()
+# def count_matrix_int(quartsnps):
+#     """
+#     return a 16x16 matrix of site counts from snparr
+#     """
+#     arr = np.zeros((16, 16), dtype=np.int64)
+#     add = np.int64(1)
+#     for idx in range(quartsnps.shape[0]):
+#         i = quartsnps[idx, :]
+#         arr[(4 * i[0]) + i[1], (4 * i[2]) + i[3]] += add
+#     return arr
 
 
 @njit()
